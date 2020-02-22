2a7d6bbb42eb31784d6e2d96b737db31aec0d1b4
saviobosco <Johnboscoomebe@yahoo.com>
2020-02-22 01:50:55 +0100

    Redirect user to dashboard after registration

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/user/dashboard/index';
 
     /**
      * Create a new controller instance.
